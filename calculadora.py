d4df114922f3c6b8b8fad0bea31f93d6e10587df
valenezka <valentina.barro18@gmail.com>
2023-05-19 16:48:19 -0300

    Calculadora original

---
+++ calculadora.py
@@ -0,0 +1,40 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
+
+print("Select operation.\n1.Add\n2.Subtract\n3.Multiply\n4.Divide\n5.Exit")
+
+while True:
+    choice = input("Enter choice(1/2/3/4/5): ")
+
+    if choice in ('1', '2', '3', '4'):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
+        break
+    elif choice == '5':
+        print("Exit")
+        break
+    else:
+        print("Invalid Input")
+
+
